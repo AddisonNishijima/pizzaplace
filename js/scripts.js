2c45e0afada369da77c9843397c869a6dc7fa2c4
Addison Nishijima <addison.nishijima@gmail.com>
2016-08-30 20:46:25 -0700

    fix js

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,25 @@ function Pizza(size, sauce, cheese, meat1, meat2, veggie1, veggie2) {
   this.meat2 = meat2;
   this.veggie1 = veggie1;
   this.veggie2 = veggie2;
-  this.price = 0;
 }
-  Pizza.prototype.pizzaPrice = function() {
-    return pizzaPrice = inputtedSize + inputtedSauce + inputtedCheese + inputtedMeatOne + inputtedMeattwo + inputtedVeggieOne + inputtedVeggieTwo + 0;
-  }
+Pizza.prototype.price = function() {
+  return this.size +
+    this.sauce +
+    this.cheese +
+    this.meat1 +
+    this.meat2 +
+    this.veggie1 +
+    this.veggie2 + 0;
+}
+
+var pizza = new Pizza(1, 1, 2, 1, 2, 3, 1);
+
+console.log(pizza)
+console.log(pizza.price()) // returns the values //
+console.log(pizza.sauce) // accesses the values //
 
 //user interface logic
-$(document).ready(function(){
+$(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $("form").fadeOut();
@@ -25,18 +36,19 @@ $(document).ready(function(){
     var inputtedMeattwo = parseInt($("#meat2").val());
     var inputtedVeggieOne = parseInt($("#veggie1").val());
     var inputtedVeggieTwo = parseInt($("#veggie2").val());
-    var sizeChoice = $( "#size option:selected" ).text();
-    var sauceChoice = $( "#sauce option:selected" ).text();
-    var cheeseChoice = $( "#cheese option:selected" ).text();
-    var meatChoiceOne = $( "#meat1 option:selected" ).text();
-    var meatChoiceTwo = $( "#meat2 option:selected" ).text();
-    var veggieChoiceOne = $( "#veggie1 option:selected" ).text();
-    var veggieChoiceTwo = $( "#veggie2 option:selected" ).text();
-    var new Pizza = Pizza();
-    var newPizza = (inputtedSize + inputtedSauce + inputtedCheese + inputtedMeatOne + inputtedMeattwo + inputtedVeggieOne + inputtedVeggieTwo);
-    Pizza.pizzaPrice(newPizza);
+    var sizeChoice = $("#size option:selected").text();
+    var sauceChoice = $("#sauce option:selected").text();
+    var cheeseChoice = $("#cheese option:selected").text();
+    var meatChoiceOne = $("#meat1 option:selected").text();
+    var meatChoiceTwo = $("#meat2 option:selected").text();
+    var veggieChoiceOne = $("#veggie1 option:selected").text();
+    var veggieChoiceTwo = $("#veggie2 option:selected").text();
+
+    var pizza = new Pizza(inputtedSize, inputtedSauce, inputtedCheese, inputtedMeatOne, inputtedMeattwo, inputtedVeggieOne, inputtedVeggieTwo);
+    //uses the pizza object//
+
     $("#total").fadeIn();
-    $(".total").text(" " + "$" + newPizza);
+    $(".total").text(" " + "$" + pizza.price()); //uses the prototype//
     $(".size").text(" " + sizeChoice);
     $(".sauce").text(" " + sauceChoice);
     $(".cheese").text(" " + cheeseChoice);
